88fa4f09d806928de5a23f2f0b8dd09ebf2e4c62
ugo <ugo.formosa.uf@gmail.com>
2024-11-27 14:22:48 +0100

    Initial commit - Projet Frenesie

---
+++ src/main/java/com/frenesie/collectif/FrenesieApplication.java
@@ -0,0 +1,13 @@
+package com.frenesie.collectif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrenesieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrenesieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frenesie/collectif/FrenesieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frenesie.collectif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrenesieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
